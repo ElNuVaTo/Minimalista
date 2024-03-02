d773cfbcbc139757dffb87604f725b3239fe4c2d
ElNuVaTo <amaroalcaq23@gmail.com>
2024-03-02 18:33:18 -0300

    BaseName url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Minimalista",
+  base: "/Minimalista/",
   server: {
     host: "0.0.0.0",
   },
